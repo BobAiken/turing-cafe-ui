c6fb6d0adb2bd92eca65c59d85b5792296fe5220
Robert Aiken <robertsiraaiken@gmail.com>
2023-02-21 12:34:23 -0600

    Add iteration 3

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,44 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('iteration 3', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+    .intercept('http://localhost:3001/api/v1/reservations',
+  )});
+
+  it('should display all elements on the page', () => {
+    cy.contains('Turing Cafe Reservations')
+    .get('form')
+    .get('div[class="resy-form"]')
+    .get('div[class="reservations-container"]')
+  })
+
+  it('when data is put into the form, the value is reflected in that form input', ()=>{
+    cy.get('input[name="name"]')
+    .type('Bobo')
+    .should('have.value', 'Bobo')
+    .get('input[name="date"]')
+    .type('2023-03-18')
+    .should('have.value', '2023-03-18')
+    .get('input[name="time"]')
+    .type('11:11')
+    .should('have.value', '11:11')
+    .get('input[name="number"]')
+    .type('1')
+    .should('have.value', '01')
+  })
+
+  it('should be able to add a new reservation to the page',()=>{
+    cy.get('input[name="name"]')
+    .type('Bobo')
+    .get('input[name="date"]')
+    .type('2023-03-18')
+    .get('input[name="time"]')
+    .type('11:11')
+    .get('input[name="number"]')
+    .type('1')
+    .get('button').click()
+    .get('div[class="reservations-container"]').children()
+    .should('have.length', 10)
+
   })
 })
